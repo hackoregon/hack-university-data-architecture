c4a042880713daba47ce5b75b0fd9b8faedf40b3
U-slewis-lenovo\slewis <slewis@slewis-lenovo.(none)>
2016-10-30 23:55:22 -0700

    Additions to install script

--- toolchain/vagrant/installPackages.sh
+++ toolchain/vagrant/installPackages.sh
@@ -1,14 +1,22 @@
-#1/bin/bash
+#!/bin/bash
 
-bash /vagrant/Miniconda3-latest-Linux-x86_64.sh -b
+echo "Installing Miniconda..."
+/vagrant/Miniconda3-latest-Linux-x86_64.sh -b
 echo "# Added for Miniconda" >> ~/.bash_profile
 echo "export PATH=\"$HOME/miniconda3/bin:\$PATH\"" >> ~/.bash_profile
 
 . ~/.bash_profile
 
+echo "Installing Jupyter..."
 conda install -y jupyter
+
+echo "Installing Pandas..."
 conda install -y pandas
+
+echo "Installing scikit and seaborn..."
 conda install -y seaborn scikit-learn
 
 mkdir .jupyter
 cp /vagrant/jupyter_notebook_config.py .jupyter
+
+echo "Installation complete" 
