5d140575a294f8f8917f39018e3d2f138f8ce79c
John Tasto <john@tasto.net>
2016-11-02 03:23:25 -0700

    Redirect output to log file in Vagrant package installation script

--- toolchain/vagrant/installPackages.sh
+++ toolchain/vagrant/installPackages.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+LOGFILE=~/installPackages.log
+
 echo "Installing Miniconda..."
-bash /vagrant/Miniconda3-latest-Linux-x86_64.sh -b
+bash /vagrant/Miniconda3-latest-Linux-x86_64.sh -b >> $LOGFILE
 echo "# Added for Miniconda" >> ~/.bash_profile
 echo "export PATH=\"$HOME/miniconda3/bin:\$PATH\"" >> ~/.bash_profile
 
 . ~/.bash_profile
 
 echo "Installing Jupyter..."
-conda install -y jupyter
+conda install -y jupyter >> $LOGFILE
 
 echo "Installing Pandas..."
-conda install -y pandas
+conda install -y pandas >> $LOGFILE
 
 echo "Installing scikit and seaborn..."
-conda install -y seaborn scikit-learn
+conda install -y seaborn scikit-learn >> $LOGFILE
 
 mkdir ~/.jupyter
 cp /vagrant/jupyter_notebook_config.py ~/.jupyter
